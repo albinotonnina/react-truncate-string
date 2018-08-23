4b6ff668b654b9d631a6e27d4dd71504ca85d3a2
Albino Tonnina <albinotonnina@gmail.com>
2018-08-23 16:59:43 +0100

    🚀 init

---
+++ README.md
@@ -0,0 +1,49 @@
+# React Truncate String
+
+> TBD
+
+## Features
+
+## Table of Contents
+
+- [Install](#install)
+- [Usage](#usage)
+- [Demos](#demos)
+- [Maintainers](#maintainers)
+- [Contribute](#contribute)
+- [License](#license)
+
+## Install
+
+```
+yarn add react-truncate-string
+```
+
+## Usage
+
+```jsx
+```
+
+## Demos
+
+Basic demo on codesandbox
+
+https://codesandbox.io/s/r4v7onrpop
+
+React-create-app Demo
+
+https://albinotonnina.github.io/demo-magic-hat
+
+https://github.com/albinotonnina/demo-magic-hat
+
+## Maintainers
+
+[@albinotonnina](https://github.com/albinotonnina)
+
+## Contribute
+
+PRs accepted.
+
+## License
+
+MIT © 2018 Albino Tonnina
---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import resolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
+import uglify from 'rollup-plugin-uglify'
+import commonJS from 'rollup-plugin-commonjs'
+import pkg from './package.json'
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      name: pkg.module,
+      file: pkg.module,
+      format: 'es'
+    },
+    {
+      name: pkg.main,
+      file: pkg.main,
+      format: 'umd',
+      sourcemap: false
+    }
+  ],
+  plugins: [
+    resolve(),
+    commonJS({
+      include: 'node_modules/**'
+    }),
+    babel({
+      presets: ['react'],
+      exclude: 'node_modules/**',
+      plugins: ['external-helpers']
+    }),
+    uglify()
+  ],
+  external: ['react', 'react-dom', 'prop-types']
+}
